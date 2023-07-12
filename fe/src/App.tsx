01967ab03b37245517b2f73aa4633d5e43d9af4c
khundi <albert5428@gmail.com>
2023-07-12 12:29:42 +0900

    ✨feat: ThemeProvider designSystem 구현, 헤더 UI 구현 (#6)

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,8 +1,17 @@
 import { useState } from 'react';
+import Header from './components/header/Header';
+import { ThemeProvider } from 'styled-components';
+import { designSystem } from './styles/designSystem.ts';
+import { GlobalStyles } from './styles/GlobalStyles.ts';
 
 export default function App() {
-  const [token, setToken] = useState(() => localStorage.getItem('token'));
+  const [token, setToken] = useState();
   const isLogin = !!token;
 
-  return <div></div>;
+  return (
+    <ThemeProvider theme={designSystem}>
+      <GlobalStyles />
+      <Header isLogin={isLogin} />
+    </ThemeProvider>
+  );
 }
