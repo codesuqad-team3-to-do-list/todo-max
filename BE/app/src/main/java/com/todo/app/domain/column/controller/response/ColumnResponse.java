9d0515c0ec0e7549924436d4cb11d2db1daf5119
jinny-l <jinny.l.dev@gmail.com>
2023-07-12 18:30:29 +0900

    ✨feat #10: ColumnResponse 객체 구현

---
+++ BE/app/src/main/java/com/todo/app/domain/column/controller/response/ColumnResponse.java
@@ -0,0 +1,37 @@
+package com.todo.app.domain.column.controller.response;
+
+import com.todo.app.domain.column.entity.Column;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ColumnResponse {
+
+    private Long columnId;
+    private String columnTitle;
+    private List<CardResponse> cards;
+
+    public ColumnResponse(Long columnId, String columnTitle, List<CardResponse> cards) {
+        this.columnId = columnId;
+        this.columnTitle = columnTitle;
+        this.cards = cards;
+    }
+
+    public static ColumnResponse from(Column column) {
+        List<CardResponse> cardResponses = column.getCards().stream()
+                .map(CardResponse::from)
+                .collect(Collectors.toList());
+        return new ColumnResponse(column.getId(), column.getTitle(), cardResponses);
+    }
+
+    public Long getColumnId() {
+        return columnId;
+    }
+
+    public String getColumnTitle() {
+        return columnTitle;
+    }
+
+    public List<CardResponse> getCards() {
+        return cards;
+    }
+}
