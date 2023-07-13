a33a243d51829c896685a1faa37f7d5ab94cc8db
jsh3418 <albert5428@gmail.com>
2023-07-13 10:40:18 +0900

    ✨feat: HistoryItem UI 구현

--- fe/src/components/history/HistoryItem.tsx
+++ fe/src/components/history/HistoryItem.tsx
@@ -1,3 +1,43 @@
+import { styled } from 'styled-components';
+
 export default function HistoryItem() {
-  return;
+  return (
+    <StyledHistory>
+      <div>
+        <StyledProfile src="https://avatars.githubusercontent.com/u/57666791?v=4" />
+      </div>
+      <div>
+        <StyledUserName>@멋진삼</StyledUserName>
+        <StyledContent>
+          블로그에 포스팅할 것을(를) 하고있는 일에서 해야할 일으로
+          이동하였습니다.
+        </StyledContent>
+        <StyledTimeStamp>3분 전</StyledTimeStamp>
+      </div>
+    </StyledHistory>
+  );
 }
+
+const StyledHistory = styled.div``;
+
+const StyledProfile = styled.img`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  border: 1px solid ${(props) => props.theme.colors.grey200};
+`;
+
+const StyledUserName = styled.div`
+  font: ${(props) => props.theme.font.displayMD14};
+  color: ${(props) => props.theme.colors.grey700};
+`;
+
+const StyledContent = styled.div`
+  font: ${(props) => props.theme.font.displayMD14};
+  color: ${(props) => props.theme.colors.grey600};
+`;
+
+const StyledTimeStamp = styled.div`
+  font: ${(props) => props.theme.font.displayMD12};
+  color: ${(props) => props.theme.colors.grey500};
+`;
