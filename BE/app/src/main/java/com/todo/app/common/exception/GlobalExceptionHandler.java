8582398aabaa5ca3ddc73aa125dbf9fa5a49f287
crtEvent <crtevn@gmail.com>
2023-07-19 23:29:06 +0900

    fix/#61: hot fix

--- BE/app/src/main/java/com/todo/app/common/exception/GlobalExceptionHandler.java
+++ BE/app/src/main/java/com/todo/app/common/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.todo.app.common.exception;
 import com.todo.app.common.ApiResponse;
 import com.todo.app.domain.jwt.controller.response.JwtExceptionResponse;
 import io.jsonwebtoken.JwtException;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -11,38 +12,45 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
-    public ApiResponse<String> handleException(Exception ex) {
+    public ApiResponse<String> handleException(Exception ex, HttpServletResponse response) {
+        response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
         return ApiResponse.exception(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
-    public ApiResponse<String> handleException(IllegalArgumentException ex) {
+    public ApiResponse<String> handleException(IllegalArgumentException ex, HttpServletResponse response) {
+        response.setStatus(HttpStatus.BAD_REQUEST.value());
         return ApiResponse.exception(HttpStatus.BAD_REQUEST, ex.getMessage());
     }
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ApiResponse<String> handleException(ResourceNotFoundException ex) {
+    public ApiResponse<String> handleException(ResourceNotFoundException ex, HttpServletResponse response) {
+        response.setStatus(ex.getHttpStatus().value());
         return ApiResponse.exception(ex.getHttpStatus(), ex.getMessage());
     }
 
     @ExceptionHandler(IllegalPasswordException.class)
-    public ApiResponse<String> handleException(IllegalPasswordException ex) {
+    public ApiResponse<String> handleException(IllegalPasswordException ex, HttpServletResponse response) {
+        response.setStatus(ex.getHttpStatus().value());
         return ApiResponse.exception(ex.getHttpStatus(), ex.getMessage());
     }
 
     @ExceptionHandler(IllegalJwtTokenException.class)
-    public ApiResponse<String> handleException(IllegalJwtTokenException ex) {
+    public ApiResponse<String> handleException(IllegalJwtTokenException ex, HttpServletResponse response) {
+        response.setStatus(ex.getHttpStatus().value());
         return ApiResponse.exception(ex.getHttpStatus(), ex.getMessage());
     }
 
     @ExceptionHandler(JwtException.class)
-    public ApiResponse<JwtExceptionResponse> handleException(JwtException ex) {
+    public ApiResponse<JwtExceptionResponse> handleException(JwtException ex, HttpServletResponse response) {
         JwtExceptionType jwtExceptionType = JwtExceptionType.from(ex);
+        response.setStatus(jwtExceptionType.getHttpStatus().value());
         return ApiResponse.exception(jwtExceptionType.getHttpStatus(), new JwtExceptionResponse(jwtExceptionType));
     }
 
     @ExceptionHandler(MemberDuplicationException.class)
-    public ApiResponse<String> handleException(MemberDuplicationException ex) {
+    public ApiResponse<String> handleException(MemberDuplicationException ex, HttpServletResponse response) {
+        response.setStatus(ex.getHttpStatus().value());
         return ApiResponse.exception(ex.getHttpStatus(), ex.getMessage());
     }
 
