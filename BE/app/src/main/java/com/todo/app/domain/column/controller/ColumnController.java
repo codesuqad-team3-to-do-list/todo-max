7b0f66c307e3f1f93ea89e6d7053d4e0a5e5dade
crtEvent <crtevn@gmail.com>
2023-07-20 14:45:24 +0900

    ✨feat/#71: ColumnController에 로그인 로직 적용

--- BE/app/src/main/java/com/todo/app/domain/column/controller/ColumnController.java
+++ BE/app/src/main/java/com/todo/app/domain/column/controller/ColumnController.java
@@ -5,6 +5,7 @@ import com.todo.app.domain.column.controller.response.ColumnResponse;
 import com.todo.app.domain.column.service.ColumnService;
 import java.util.List;
 import java.util.stream.Collectors;
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,9 +20,9 @@ public class ColumnController {
     }
 
     @GetMapping("/api/columns")
-    public ApiResponse<List<ColumnResponse>> readColumns() {
-        List<ColumnResponse> columnResponses = columnService.readAll(1L)
-                .stream()// TODO: 로그인 기능 구현 후 readAll() 파라미터 수정 필요
+    public ApiResponse<List<ColumnResponse>> readColumns(HttpServletRequest request) {
+        List<ColumnResponse> columnResponses = columnService.readAll(Long.valueOf(String.valueOf(request.getAttribute("memberId"))))
+                .stream() // TODO: 로그인 기능 구현 후 readAll() 파라미터 수정 필요
                 .map(ColumnResponse::from)
                 .collect(Collectors.toList());
         return ApiResponse.success(HttpStatus.OK, columnResponses);
